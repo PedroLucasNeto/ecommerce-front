cf3bbebdeb0046a90433d389c0bc0f13e129071b
Harlon Garcia <harlondesigner@gmail.com>
2023-06-22 03:00:28 -0300

    feat: add avatar and user basic info

--- src/pages/users/[id].tsx
+++ src/pages/users/[id].tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import * as Avatar from '@radix-ui/react-avatar';
 
 const fakeUser = {
   id: 1,
-  name: 'John Doe',
+  name: 'Harlon',
   email: 'johndoe@gmail.com',
-  photoUrl: '',
+  avatarUrl: '',
   adresses: [
     {
       street: 'Rua da Alegria',
@@ -27,8 +28,24 @@ const fakeUser = {
 
 export default function MyAccount() {
   return (
-    <main>
-      <div>{fakeUser.name}</div>
+    <main className="h-screen p-8">
+      <section className="flex gap-4">
+        <Avatar.Root className="flex h-16 w-16 rounded-full overflow-hidden">
+          <Avatar.Image
+            src={fakeUser.avatarUrl}
+            alt={fakeUser.name}
+            className="h-full w-full"
+          />
+          <Avatar.Fallback className="flex items-center justify-center h-full w-full text-base text-slate-950 bg-slate-200">
+            {fakeUser.name[0]}
+          </Avatar.Fallback>
+        </Avatar.Root>
+
+        <div className="flex flex-col">
+          <strong>{fakeUser.name}</strong>
+          <small>{fakeUser.email}</small>
+        </div>
+      </section>
     </main>
   );
 }
