f38135e2c5279d6e9ed43767677724440811a75b
Harlon Garcia <harlondesigner@gmail.com>
2023-06-23 18:07:46 -0300

    feat: add profile option

--- src/pages/users/[id].tsx
+++ src/pages/users/[id].tsx
@@ -1,50 +1,39 @@
 import React from 'react';
 import * as Avatar from '@radix-ui/react-avatar';
-
-const fakeUser = {
-  id: 1,
-  name: 'Harlon',
-  email: 'johndoe@gmail.com',
-  avatarUrl: '',
-  adresses: [
-    {
-      street: 'Rua da Alegria',
-      number: '204',
-      additionalComplement: 'Do lado da Rua da Felicidade',
-      city: 'Campina Grande',
-      state: 'PB',
-      cep: '58400-000',
-    },
-    {
-      street: 'Rua da Alegria',
-      number: '204',
-      additionalComplement: 'Do lado da Rua da Felicidade',
-      city: 'Campina Grande',
-      state: 'PB',
-      cep: '58400-000',
-    },
-  ],
-};
+import * as Separator from '@radix-ui/react-separator';
+import ProfileOption from '@/components/ProfileOption';
+import { fakeUser } from '@/utils/data';
 
 export default function MyAccount() {
   return (
     <main className="h-screen p-8">
       <section className="flex gap-4">
-        <Avatar.Root className="flex h-16 w-16 rounded-full overflow-hidden">
+        <Avatar.Root className="flex h-14 w-14 rounded-full overflow-hidden">
           <Avatar.Image
             src={fakeUser.avatarUrl}
             alt={fakeUser.name}
-            className="h-full w-full"
+            className="h-full w-full pointer-events-none"
           />
-          <Avatar.Fallback className="flex items-center justify-center h-full w-full text-base text-slate-950 bg-slate-200">
+          <Avatar.Fallback
+            className="flex items-center justify-center h-full w-full text-base  
+          font-bold text-slate-900 bg-orange-500"
+          >
             {fakeUser.name[0]}
           </Avatar.Fallback>
         </Avatar.Root>
-
         <div className="flex flex-col">
           <strong>{fakeUser.name}</strong>
           <small>{fakeUser.email}</small>
         </div>
+      </section>
+      <Separator.Root
+        orientation="horizontal"
+        className="h-[1px] w-full my-4 bg-slate-300 md:bg-slate-200"
+      />
+      <section className="flex flex-col gap-2">
+        {[1, 2, 3, 4].map((index) => (
+          <ProfileOption key={index} />
+        ))}
       </section>
     </main>
   );
---
+++ src/utils/data.ts
@@ -0,0 +1,24 @@
+export const fakeUser = {
+  id: 1,
+  name: 'Harlon',
+  email: 'johndoe@gmail.com',
+  avatarUrl: 'https://avatars.githubusercontent.com/u/89430601?v=4',
+  adresses: [
+    {
+      street: 'Rua da Alegria',
+      number: '204',
+      additionalComplement: 'Do lado da Rua da Felicidade',
+      city: 'Campina Grande',
+      state: 'PB',
+      cep: '58400-000',
+    },
+    {
+      street: 'Rua da Alegria',
+      number: '204',
+      additionalComplement: 'Do lado da Rua da Felicidade',
+      city: 'Campina Grande',
+      state: 'PB',
+      cep: '58400-000',
+    },
+  ],
+};
